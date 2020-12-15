6d3493285d131235b157d127eec002f8c6b795cb
Emil Zainagabdinov <screw192@gmail.com>
2020-12-15 13:39:11 +0600

    Added Card.js component, CardDeck.js and PokerHand.js files. Added render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Card from './components/Card/Card';
+import CardDeck from "./CardDeck";
+import PokerHand from "./PokerHand";
 import './App.css';
+import './card.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    cardsOnHand: [],
+    hand: '',
+  };
 
-    </div>
-  );
+  getNewCards = async () => {
+    await this.setState({
+      cardsOnHand: new CardDeck().getCards(5)
+    })
+    this.setState({
+      hand: new PokerHand(this.state.cardsOnHand).getOutcome()
+    })
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <button className="cardButton" onClick={this.getNewCards}>Get cards</button>
+          <Card cards={this.state.cardsOnHand} />
+          <h4 className="pokerHand">{this.state.hand}</h4>
+        </div>
+    );
+  }
 }
 
 export default App;
